babc0381b76046daaa8e9c2a71007014f74a1f7d
Kirill Lyashenko <tltelf@yandex.ru>
2023-08-24 20:38:54 +0400

    add: Experience section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Intro from '../components/Intro';
 import About from '@/components/About';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
+import Experience from '@/components/Experience';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Experience />
     </main>
   )
 }
---
+++ components/Experience.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import React from 'react'
+import SectionHeading from './SectionHeading'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { experiencesData } from '@/lib/data';
+import { useSectionInView } from '@/lib/hooks';
+
+export default function Experience() {
+  const { ref } = useSectionInView('Experience', 0.5);
+
+  return (
+    <section ref={ref} id='experience'>
+      <SectionHeading>My experience</SectionHeading>
+      <VerticalTimeline lineColor=''>
+        {
+          experiencesData.map((item, index) => (
+            <VerticalTimelineElement key={index}
+              className="vertical-timeline-element--work"
+              contentStyle={{ 
+                background: '#f3f4f6', 
+                boxShadow: 'none',
+                border: '1px solid rgba(0, 0, 0, 0.05)',
+                textAlign: 'left',
+                padding: '1.3rem 2rem',  
+              }}
+              contentArrowStyle={{ 
+                borderRight: '0.4rem solid  #9ca3af' }}
+              date={item.date}
+              iconStyle={{ background: 'white', fontSize: '1.5rem' }}
+              icon={item.icon}
+            >
+              <h3 className='font-semibold capitalize'>{item.title}</h3>
+              <p className='font-normal !mt-0'>{item.location}</p>
+              <p className='!mt-1 !font-normal text-gray-700'>{item.description}</p>
+            </VerticalTimelineElement>
+          ))
+        }
+      </VerticalTimeline>
+    </section>
+  )
+}
